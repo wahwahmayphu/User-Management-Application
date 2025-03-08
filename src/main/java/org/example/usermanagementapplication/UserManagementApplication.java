a11d6bef83e8aeede70e0777c5697a3bf061e032
wahwahmayphu <wahwahmayphu1997@gmail.com>
2025-03-08 18:41:21 +0630

    added BCryptPasswordEncoder method

--- src/main/java/org/example/usermanagementapplication/UserManagementApplication.java
+++ src/main/java/org/example/usermanagementapplication/UserManagementApplication.java
@@ -2,12 +2,18 @@ package org.example.usermanagementapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class UserManagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserManagementApplication.class, args);
+    }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
