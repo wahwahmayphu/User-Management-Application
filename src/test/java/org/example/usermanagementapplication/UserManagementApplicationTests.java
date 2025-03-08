7d6411c9942256ffad8624e4be88d2af9d722315
wahwahmayphu <wahwahmayphu1997@gmail.com>
2025-03-08 17:15:49 +0630

    initial project set up

---
+++ src/test/java/org/example/usermanagementapplication/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.usermanagementapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
